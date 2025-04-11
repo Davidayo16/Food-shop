de4c2b0d2d979b08666bf78f195d99d1ad90ff20
Davidayo16 <idowuodimayo@gmail.com>
2025-04-11 15:44:38 +0100

    uodate

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -79,7 +79,7 @@ export function SiteHeader() {
               </span>
             </Button>
           </Link>
-          <LoginSimulator />
+          {/* <LoginSimulator /> */}
         </div>
       </div>
     </header>
